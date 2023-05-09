be28a9fb826463ffe165591f0df9881432b16f4d
yisus043 <111025279+yisus043@users.noreply.github.com>
2023-05-09 17:44:00 -0600

    Update DBconect.php

--- DBconect.php
+++ DBconect.php
@@ -1,1 +1,20 @@
 
+<?php
+$db_host="127.0.0.1"; //localhost server 
+$db_user="u995420991_sistemadigital";	//database username
+$db_password="U995420991_sistemadigital";	//database password   
+$db_name="u995420991_sistemadigital";	//database name
+
+try
+{
+	$db=new PDO("mysql:host={$db_host};dbname={$db_name}",$db_user,$db_password);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOEXCEPTION $e)
+{
+	$e->getMessage();
+}
+
+?>
+
+
